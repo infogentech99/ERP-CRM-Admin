a9697363aa18c575a8c8fca8136430dcba02af0b
infogentech99 <infogentech99@gmail.com>
2025-03-27 17:23:56 +0530

    commit 7

--- app.js
+++ app.js
@@ -1,31 +1,123 @@
+// const express = require("express");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const promisify = require("es6-promisify");
+
+// const apiRouter = require("./routes/api");
+// const authApiRouter = require("./routes/authApi");
+
+// const errorHandlers = require("./handlers/errorHandlers");
+
+// const { isValidToken } = require("./controllers/authController");
+
+// require("dotenv").config({ path: ".variables.env" });
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(
+//   session({
+//     secret: process.env.SECRET,
+//     key: process.env.KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+//   })
+// );
+
+// app.use((req, res, next) => {
+//   res.locals.admin = req.admin || null;
+//   res.locals.currentPath = req.path;
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   req.login = promisify(req.login, req);
+//   next();
+// });
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
+//   res.header("Access-Control-Expose-Headers", "Content-Length");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
+//   );
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   } else {
+//     return next();
+//   }
+// });
+
+// app.use("/api", authApiRouter);
+
+// app.use("/api", apiRouter);
+// app.use(errorHandlers.notFound);
+
+// if (app.get("env") === "development") {
+//   app.use(errorHandlers.developmentErrors);
+// }
+// app.use(errorHandlers.productionErrors);
+// module.exports = app;
+
+
+
+
+
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const promisify = require("es6-promisify");
 
 const apiRouter = require("./routes/api");
 const authApiRouter = require("./routes/authApi");
 
 const errorHandlers = require("./handlers/errorHandlers");
-
 const { isValidToken } = require("./controllers/authController");
 
 require("dotenv").config({ path: ".variables.env" });
 
-// create our Express app
 const app = express();
 
-// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
+// ✅ Setup CORS properly
+const allowedOrigins = [
+  "https://crm-admin-nu.vercel.app", // your frontend domain
+  "http://localhost:3000",           // optional for local dev
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS Not Allowed"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Takes the raw requests and turns them into usable properties on req.body
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Sessions allow us to Contact data on visitors from request to request
-// This keeps admins logged in and allows us to send flash messages
+// Sessions
 app.use(
   session({
     secret: process.env.SECRET,
@@ -33,58 +125,33 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    },
   })
 );
 
-// pass variables to our templates + all requests
+// Add login support + locals
 app.use((req, res, next) => {
   res.locals.admin = req.admin || null;
   res.locals.currentPath = req.path;
-  next();
-});
-
-// promisify some callback based APIs
-app.use((req, res, next) => {
   req.login = promisify(req.login, req);
   next();
 });
 
-// Here our API Routes
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
-  res.header("Access-Control-Expose-Headers", "Content-Length");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  } else {
-    return next();
-  }
-});
-
+// Routes
 app.use("/api", authApiRouter);
-
-// for development & production don't use this line app.use("/api", apiRouter); , this is just demo login contoller
 app.use("/api", apiRouter);
 
-//uncomment line below // app.use("/api", isValidToken, apiRouter);
-// app.use("/api", isValidToken, apiRouter);
-
-// If that above routes didnt work, we 404 them and forward to error handler
+// 404 handler
 app.use(errorHandlers.notFound);
 
-// Otherwise this was a really bad error we didn't expect! Shoot eh
+// Error handler
 if (app.get("env") === "development") {
-  /* Development Error Handler - Prints stack trace */
   app.use(errorHandlers.developmentErrors);
 }
-
-// production error handler
 app.use(errorHandlers.productionErrors);
 
-// done! we export it so we can start the site in start.js
 module.exports = app;
