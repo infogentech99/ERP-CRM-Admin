7f2ee471ee3e909f36c565abf6ed621325e57f83
infogentech99 <infogentech99@gmail.com>
2025-03-27 16:32:59 +0530

    commit 4

--- app.js
+++ app.js
@@ -1,176 +1,3 @@
-// // const express = require("express");
-// // const session = require("express-session");
-// // const MongoStore = require("connect-mongo");
-// // const path = require("path");
-// // const bodyParser = require("body-parser");
-// // const promisify = require("es6-promisify");
-
-// // const apiRouter = require("./routes/api");
-// // const authApiRouter = require("./routes/authApi");
-
-// // const errorHandlers = require("./handlers/errorHandlers");
-
-// // const { isValidToken } = require("./controllers/authController");
-
-// // require("dotenv").config({ path: ".variables.env" });
-
-// // // create our Express app
-// // const app = express();
-
-// // // serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-// // app.use(express.static(path.join(__dirname, "public")));
-
-// // // Takes the raw requests and turns them into usable properties on req.body
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: true }));
-
-// // // Sessions allow us to Contact data on visitors from request to request
-// // // This keeps admins logged in and allows us to send flash messages
-// // app.use(
-// //   session({
-// //     secret: process.env.SECRET,
-// //     key: process.env.KEY,
-// //     resave: false,
-// //     saveUninitialized: false,
-// //     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
-// //   })
-// // );
-
-// // // pass variables to our templates + all requests
-// // app.use((req, res, next) => {
-// //   res.locals.admin = req.admin || null;
-// //   res.locals.currentPath = req.path;
-// //   next();
-// // });
-
-// // // promisify some callback based APIs
-// // app.use((req, res, next) => {
-// //   req.login = promisify(req.login, req);
-// //   next();
-// // });
-
-// // // Here our API Routes
-// // app.use(function (req, res, next) {
-// //   res.header("Access-Control-Allow-Origin", "*");
-// //   res.header("Access-Control-Allow-Credentials", "true");
-// //   res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
-// //   res.header("Access-Control-Expose-Headers", "Content-Length");
-// //   res.header(
-// //     "Access-Control-Allow-Headers",
-// //     "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
-// //   );
-// //   if (req.method === "OPTIONS") {
-// //     return res.sendStatus(200);
-// //   } else {
-// //     return next();
-// //   }
-// // });
-
-// // app.use("/api", authApiRouter);
-
-// // // for development & production don't use this line app.use("/api", apiRouter); , this is just demo login contoller
-// // app.use("/api", apiRouter);
-
-// // //uncomment line below // app.use("/api", isValidToken, apiRouter);
-// // // app.use("/api", isValidToken, apiRouter);
-
-// // // If that above routes didnt work, we 404 them and forward to error handler
-// // app.use(errorHandlers.notFound);
-
-// // // Otherwise this was a really bad error we didn't expect! Shoot eh
-// // if (app.get("env") === "development") {
-// //   /* Development Error Handler - Prints stack trace */
-// //   app.use(errorHandlers.developmentErrors);
-// // }
-
-// // // production error handler
-// // app.use(errorHandlers.productionErrors);
-
-// // // done! we export it so we can start the site in start.js
-// // module.exports = app;
-
-
-
-// require("dotenv").config({ path: ".variables.env" });
-
-// const express = require("express");
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
-// const path = require("path");
-// const bodyParser = require("body-parser");
-// const promisify = require("es6-promisify");
-
-// const apiRouter = require("./routes/api");
-// const authApiRouter = require("./routes/authApi");
-// const errorHandlers = require("./handlers/errorHandlers");
-// const { isValidToken } = require("./controllers/authController");
-
-// const app = express();
-
-// // Static files
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // Body parser
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Sessions
-// app.use(
-//   session({
-//     secret: process.env.SECRET || "my-secret-key",
-//     key: process.env.KEY || "sessionId",
-//     resave: false,
-//     saveUninitialized: false,
-//     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
-//     cookie: {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production",
-//       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-//     },
-//   })
-// );
-
-// // Global middleware
-// app.use((req, res, next) => {
-//   res.locals.admin = req.admin || null;
-//   res.locals.currentPath = req.path;
-//   req.login = promisify(req.login, req);
-//   next();
-// });
-
-// // CORS
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
-//   res.header("Access-Control-Expose-Headers", "Content-Length");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
-//   );
-//   if (req.method === "OPTIONS") return res.sendStatus(200);
-//   next();
-// });
-
-// // Routes
-// app.use("/auth", authApiRouter);         // Public routes like login/register
-// app.use("/api", isValidToken, apiRouter); // Protected routes
-
-// // Error handlers
-// app.use(errorHandlers.notFound);
-// if (app.get("env") === "development") {
-//   app.use(errorHandlers.developmentErrors);
-// } else {
-//   app.use(errorHandlers.productionErrors);
-// }
-
-// module.exports = app;
-
-
-
-// Load environment variables
-require("dotenv").config({ path: ".variables.env" });
-
 const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
@@ -178,47 +5,52 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const promisify = require("es6-promisify");
 
-// Routes and controllers
 const apiRouter = require("./routes/api");
 const authApiRouter = require("./routes/authApi");
+
 const errorHandlers = require("./handlers/errorHandlers");
+
 const { isValidToken } = require("./controllers/authController");
 
+require("dotenv").config({ path: ".variables.env" });
+
+// create our Express app
 const app = express();
 
-// Serve static files
+// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, "public")));
 
-// Parse request bodies
+// Takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Session handling
+// Sessions allow us to Contact data on visitors from request to request
+// This keeps admins logged in and allows us to send flash messages
 app.use(
   session({
-    secret: process.env.SECRET || "my-secret-key",
-    key: process.env.KEY || "sessionId",
+    secret: process.env.SECRET,
+    key: process.env.KEY,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
-    cookie: {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    },
   })
 );
 
-// Global middleware for template variables and promisify
+// pass variables to our templates + all requests
 app.use((req, res, next) => {
   res.locals.admin = req.admin || null;
   res.locals.currentPath = req.path;
+  next();
+});
+
+// promisify some callback based APIs
+app.use((req, res, next) => {
   req.login = promisify(req.login, req);
   next();
 });
 
-// CORS headers
-app.use((req, res, next) => {
+// Here our API Routes
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,PATCH,PUT,POST,DELETE");
@@ -227,20 +59,32 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Accept, Authorization,x-auth-token, Content-Type, X-Requested-With, Range"
   );
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    return next();
+  }
 });
 
-// Auth and API routes
-app.use("/auth", authApiRouter);         // Public routes like login/register
-app.use("/api", isValidToken, apiRouter); // Protected routes
+app.use("/api", authApiRouter);
 
-// Error handlers
+// for development & production don't use this line app.use("/api", apiRouter); , this is just demo login contoller
+app.use("/api", apiRouter);
+
+//uncomment line below // app.use("/api", isValidToken, apiRouter);
+// app.use("/api", isValidToken, apiRouter);
+
+// If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
+
+// Otherwise this was a really bad error we didn't expect! Shoot eh
 if (app.get("env") === "development") {
+  /* Development Error Handler - Prints stack trace */
   app.use(errorHandlers.developmentErrors);
-} else {
-  app.use(errorHandlers.productionErrors);
 }
 
+// production error handler
+app.use(errorHandlers.productionErrors);
+
+// done! we export it so we can start the site in start.js
 module.exports = app;
--- server.js
+++ server.js
@@ -1,80 +1,39 @@
-// const mongoose = require("mongoose");
-
-// // Make sure we are running node 10.0+
-// const [major, minor] = process.versions.node.split(".").map(parseFloat);
-// if (major < 10 || (major === 10 && minor <= 0)) {
-//   console.log(
-//     "Please go to nodejs.org and download version 10 or greater. 👌\n "
-//   );
-//   process.exit();
-// }
-
-// // import environmental variables from our variables.env file
-// require("dotenv").config({ path: ".variables.env" });
-
-
-// mongoose.connect(process.env.DATABASE, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true,
-// });
-// mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-// mongoose.connection.on("error", (err) => {
-//   console.error(`🚫 Error → : ${err.message}`);
-// });
-
-// const glob = require("glob");
-// const path = require("path");
-
-// glob.sync("./models/*.js").forEach(function (file) {
-//   require(path.resolve(file));
-// });
-
-// // Start our app!
-// const app = require("./app");
-// app.set("port", process.env.PORT || 80);
-// const server = app.listen(app.get("port"), () => {
-//   console.log(`Express running → On PORT : ${server.address().port}`);
-// });
-
-
-
 const mongoose = require("mongoose");
-const glob = require("glob");
-const path = require("path");
-const app = require("./app");
 
-// ✅ 1. Node version check
+// Make sure we are running node 10.0+
 const [major, minor] = process.versions.node.split(".").map(parseFloat);
 if (major < 10 || (major === 10 && minor <= 0)) {
-  console.error("🚫 Please upgrade Node.js to version 10 or higher.");
+  console.log(
+    "Please go to nodejs.org and download version 10 or greater. 👌\n "
+  );
   process.exit();
 }
 
-// ✅ 2. Load environment variables
+// import environmental variables from our variables.env file
 require("dotenv").config({ path: ".variables.env" });
 
-// ✅ 3. Connect to MongoDB
+
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,  // Optional in newer versions
-  useCreateIndex: true,     // Optional in newer versions
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on("error", (err) => {
-  console.error(`🚫 MongoDB connection error → ${err.message}`);
+  console.error(`🚫 Error → : ${err.message}`);
 });
 
-// ✅ 4. Auto-load models
-glob.sync("./models/*.js").forEach((file) => {
+const glob = require("glob");
+const path = require("path");
+
+glob.sync("./models/*.js").forEach(function (file) {
   require(path.resolve(file));
 });
 
-// ✅ 5. Start the app
-const PORT = process.env.PORT || 80;
-app.set("port", PORT);
-const server = app.listen(PORT, () => {
-  console.log(`✅ Express running → On PORT : ${server.address().port}`);
+// Start our app!
+const app = require("./app");
+app.set("port", process.env.PORT || 80);
+const server = app.listen(app.get("port"), () => {
+  console.log(`Express running → On PORT : ${server.address().port}`);
 });
